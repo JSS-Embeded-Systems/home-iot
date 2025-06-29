33cc254fb5c9b93db8562d7bf1f9148c513c04bf
Nils Jacobi <76872969+WhisperN@users.noreply.github.com>
2025-06-29 20:27:44 +0200

    Changes to Docker env

--- iot-backend/src/main/java/iot/microservice/server/ServerApplication.java
+++ iot-backend/src/main/java/iot/microservice/server/ServerApplication.java
@@ -2,12 +2,12 @@ package iot.microservice.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages="iot.microservice")
+@EnableMongoRepositories(basePackages = "iot.microservice.repository")
 public class ServerApplication {
 
 	public static void main(String[] args) {
