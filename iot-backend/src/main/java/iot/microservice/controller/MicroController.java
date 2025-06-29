38bae525b12dcc129f9442146a50d2003597e5cd
Nils Jacobi <76872969+WhisperN@users.noreply.github.com>
2025-06-29 20:29:38 +0200

    Changes to Docker env

--- iot-backend/src/main/java/iot/microservice/controller/MicroController.java
+++ iot-backend/src/main/java/iot/microservice/controller/MicroController.java
@@ -4,18 +4,26 @@ package iot.microservice.controller;
  * API V1
  */
 
-import iot.microservice.service.Service;
+import iot.microservice.service.MicroService;
 import iot.microservice.structure.RGBRequest;
 import iot.microservice.structure.Response;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/rest")
-class MicroserviceRestController {
-  //Post Requests
-  private Service service;
+@RequiredArgsConstructor
+public class MicroController {
+
+  private final MicroService service;
+
+  @PostConstruct
+  public void init() {
+    System.out.println("✅ MicroserviceRestController wurde geladen");
+  }
 
   /**
    *
@@ -29,13 +37,17 @@ class MicroserviceRestController {
     return new ResponseEntity<>(res, HttpStatus.OK);
   }
 
-
-  @PostMapping("bedroom/lamp/{val}")
+  @PostMapping("/bedroom/lamp/{val}")
   public ResponseEntity<Response> lamp(@PathVariable String val) {
     Response res = new Response();
     res.setMessage(service.shellyRelaySwitch(val));
     return new ResponseEntity<>(res, HttpStatus.OK);
   }
 
-
+  @GetMapping("/bedroom/sleep/{day}")
+  public ResponseEntity<Response> sleep(@PathVariable("day") int timeframe) {
+    Response res = new Response();
+    res.setMessage(service.retrieveSleep("sensor/sleep",timeframe));
+    return new ResponseEntity<>(res, HttpStatus.OK);
+  }
 }
